6077ea187c8c8a603ad967f29faa5f9967d864cf
Alexander Hellemans <164719002+hung4794@users.noreply.github.com>
2025-06-13 15:26:06 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+install_path="/usr/local/bin/site"
+run_cmd="site"
+
+echo "正在下載腳本..."
+wget -qO "$install_path" https://gitlab.com/gebu8f/sh/-/raw/main/nginx/ng.sh || {
+  echo "下載失敗，請檢查網址或網路狀態。"
+  exit 1
+}
+
+chmod +x "$install_path"
+
+
+echo
+echo "腳本已成功安裝！"
+echo "請輸入 '$run_cmd' 啟動面板。"
+
+read -n 1 -s -r -p "按任意鍵立即啟動..." key
+echo
+"site"
